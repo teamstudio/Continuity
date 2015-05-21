8f147a0a463179243d82ec7efb2c1374d239b276
Mark Leusink <m.leusink@gmail.com>
2015-05-21 11:38:44 +0200

    loading indicator for partial refreshes added

--- ContinuityApp/Code/ScriptLibraries/genericFunctions.js
+++ ContinuityApp/Code/ScriptLibraries/genericFunctions.js
@@ -47,3 +47,110 @@ function toggleDetails( idImg, idDetails ) {
 	}
 	
 }
+
+//Modified version of 'Standby Dialog V.3.2'
+//Code Compile by Fredrik Norling www.xpagedeveloper.com
+//used with permission from the authors of the original code
+//http://dontpanic82.blogspot.com/2010/01/xpages-hijackingpublishing-partial.html
+//http://lotusnotus.com/lotusnotus_en.nsf/dx/xpages-tip-a-modal-waiting-dialog-for-background-processes..htm
+var init_hijackAndPublishPartialRefresh=false;
+
+if(typeof hijackAndPublishPartialRefresh != 'function')
+{ 
+	//load only once
+	init_hijackAndPublishPartialRefresh=true;
+	var obj_hijackAndPublishPartialRefresh=function (){
+		
+		// hijack the partial refresh 
+		XSP._inheritedPartialRefresh = XSP._partialRefresh;
+		
+		XSP._partialRefresh = function( method, form, refreshId, options ){  
+			// Publish init
+			dojo.publish( 'partialrefresh-init', [ method, form, refreshId, options ]);
+			this._inheritedPartialRefresh( method, form, refreshId, options );
+		}
+
+		// Publish start, complete and error states 
+		dojo.subscribe( 'partialrefresh-init', function( method, form, refreshId, options ){
+
+			if( options ){ // Store original event handlers
+				var eventOnStart = options.onStart; 
+				var eventOnComplete = options.onComplete;
+				var eventOnError = options.onError;
+			}
+	
+			options = options || {};
+			
+			options.onStart = function(){
+				dojo.publish( 'partialrefresh-start', [ method, form, refreshId, options ]);
+				if( eventOnStart ){
+					if( typeof eventOnStart === 'string' ){
+						eval(eventOnStart)
+					} else {
+						eventOnStart();
+					}
+				}
+			};
+	
+			options.onComplete = function(){
+				dojo.publish( 'partialrefresh-complete', [ method, form, refreshId, options ]);
+				if( eventOnComplete ){
+					if( typeof eventOnComplete === 'string' ){
+						eval( eventOnComplete );
+					} else {
+						eventOnComplete();
+					}
+				}
+			};
+	
+			options.onError = function(){
+				dojo.publish( 'partialrefresh-error', [ method, form, refreshId, options ]);
+				if( eventOnError ){
+					if( typeof eventOnError === 'string' ){
+						eval( eventOnError );
+					} else {
+						eventOnError();
+					}
+				}
+			};
+		});
+	}
+	
+	hijackAndPublishPartialRefresh = obj_hijackAndPublishPartialRefresh;
+	
+}
+
+var doShowLoading = false;
+
+function showLoading() {
+	if (doShowLoading) {
+		dojo.style( 'loading', 'display', 'block');
+	}
+}
+
+function enableLoadingIndicator() {
+	try{
+		//dojo-subscribe('onfocus',null,function(method,form,refreshId){FieldOnfocus()})
+		dojo.subscribe( 'partialrefresh-start', null, function( method, form, refreshId ){
+			doShowLoading = true;
+			setTimeout( showLoading, 200);
+		
+		} );
+		dojo.subscribe( 'partialrefresh-complete', null, function( method, form, refreshId ){
+			doShowLoading = false;
+			dojo.style( 'loading', 'display', 'none');	   
+		} );
+		
+		dojo.subscribe( 'partialrefresh-error', null, function( method, form, refreshId ){
+		  alert("An error occurred...");
+		} );
+	}catch(e){
+		console.log(e)
+	}
+}
+
+if(init_hijackAndPublishPartialRefresh==true){
+	hijackAndPublishPartialRefresh();
+}
+
+dojo.addOnLoad( enableLoadingIndicator );
